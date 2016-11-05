5243c7820bd7013d4bbecd75964b708ec42dd4ac
BYLIN2016 <1685025511@qq.com>
2016-11-05 13:03:57 +0800

    myfirstmvc

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(myMVC.Startup))]
+namespace myMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
